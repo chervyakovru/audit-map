4960c3353764407629c4b31318fef170271106f7
Daniil Chervyakov <2yodi@mail.ru>
2019-12-22 00:07:56 +0500

    add menu component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "normalize.css";
+import UIkit from "uikit";
+import Icons from "uikit/dist/js/uikit-icons";
+import "uikit/dist/css/uikit.css";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+UIkit.use(Icons);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
