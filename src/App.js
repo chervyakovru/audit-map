630bbf488bc3a192a6c274c3955dd421dbc5b6e6
Daniil Chervyakov <2yodi@mail.ru>
2020-01-20 02:15:07 +0500

    add map example

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React from 'react';
-import Menu from './Menu';
 
-function App() {
+import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
+
+import map from './map.jpg';
+
+const handleKeyPress = () => {};
+
+const App = () => {
   return (
-    <div className="uk-height-1-1 uk-background-muted">
-      <Menu />
+    <div
+      style={{
+        width: '100%',
+        height: '100%',
+        position: 'relative'
+      }}
+    >
+      <TransformWrapper
+        options={{
+          limitToBounds: false
+        }}
+        defaultScale={1}
+        defaultPositionX={200}
+        defaultPositionY={100}
+        style={{ width: '100%', height: '100%' }}
+      >
+        <TransformComponent>
+          <div
+            onClick={e => alert(`${e.clientX}, ${e.clientY}`)}
+            onKeyPress={handleKeyPress}
+            role="main"
+            style={{
+              width: '475px',
+              height: '300px'
+            }}
+          >
+            <img
+              src={map}
+              alt="map"
+              style={{
+                border: '1px solid gray'
+              }}
+            />
+          </div>
+        </TransformComponent>
+      </TransformWrapper>
     </div>
   );
-}
+};
 
 export default App;
