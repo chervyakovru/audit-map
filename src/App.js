c1a319d2a23dc1bcf9c9eb92bd23a7a296f752b5
Daniil Chervyakov <2yodi@mail.ru>
2020-01-22 03:16:06 +0500

    remove styled-components, add react-icon

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+
 import Map from './Map';
 
 const App = () => {
-  return <Map />;
+  return (
+    <div className="main">
+      <Map />
+    </div>
+  );
 };
 
 export default App;
