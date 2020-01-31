ba1f8a8156895478c192f609abe79513dfb49ac2
Daniil Chervyakov <2yodi@mail.ru>
2020-02-01 02:49:11 +0500

    add routing and dashboard

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import StoreContext from 'storeon/react/context';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom';
 import store from './store';
 
-import Map from './Map';
-import Modal from './Modal';
+import Board from './Board';
+import Dashboard from './Dashboard';
+
+const PublicHomePage = () => {
+  return null;
+};
 
 const App = () => {
+  const loggedIn = true;
+
   return (
-    <div className="main">
-      <Map />
-      <Modal />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          {loggedIn ? <Redirect to="/dashboard" /> : <PublicHomePage />}
+        </Route>
+        <Route path="/dashboard">
+          <Dashboard />
+        </Route>
+        <Route path="/board/:id">
+          <Board />
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
---
+++ src/Board/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Map from '../Map';
+import Modal from '../Modal';
+
+const Board = () => {
+  return (
+    <div className="main">
+      <Map />
+      <Modal />
+    </div>
+  );
+};
+
+export default Board;
