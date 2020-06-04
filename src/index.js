f482fb078839f7b984ad13ad1f41c0c38c135de7
Daniil Chervyakov <daniil@chervyakov.ru>
2020-06-05 03:05:49 +0500

    remove serviceworker

--- src/index.js
+++ src/index.js
@@ -6,13 +6,7 @@ import Icons from 'uikit/dist/js/uikit-icons';
 import 'uikit/dist/css/uikit.css';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 UIkit.use(Icons);
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
