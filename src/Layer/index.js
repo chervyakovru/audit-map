71b30e97c36947fa9c6ae1a7929224f45708df67
Daniil Chervyakov <daniil@chervyakov.ru>
2020-07-02 23:01:49 +0500

    add rotate function

--- src/Layer/index.js
+++ src/Layer/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import useStoreon from 'storeon/react';
 
+import { MdRotateRight } from 'react-icons/md';
 import { getFileRef, getLayersCollection, getPointsCollection } from '../api';
 
 import UploadFile from '../Board/UploadFile';
 import Map from '../Map';
+import Button from '../Button';
 
 const getImageSize = (w, h) => {
   const { clientWidth, clientHeight } = document.documentElement;
@@ -103,6 +105,56 @@ const Layer = () => {
       });
   }, [layerId]);
 
+  function rotate(url) {
+    return new Promise(resolve => {
+      const tmpImg = new Image();
+      tmpImg.crossOrigin = 'anonymous';
+      tmpImg.addEventListener('load', () => {
+        const canvas = document.createElement('canvas');
+        canvas.width = tmpImg.height;
+        canvas.height = tmpImg.width;
+
+        const ctx = canvas.getContext('2d');
+        ctx.transform(0, 1, -1, 0, tmpImg.height, 0);
+        ctx.drawImage(tmpImg, 0, 0, tmpImg.width, tmpImg.height);
+
+        canvas.toBlob(blob => {
+          const newImageUrl = URL.createObjectURL(blob);
+
+          resolve({ src: newImageUrl, width: canvas.width, height: canvas.height });
+        });
+      });
+      tmpImg.src = url;
+    });
+  }
+
+  const handleChange = async () => {
+    const data = await rotate(image.data.src);
+    const imageSize = getImageSize(data.width, data.height);
+
+    setImage({
+      data: {
+        src: data.src,
+        ...imageSize,
+      },
+      loaded: true,
+      exists: true,
+    });
+
+    getPointsCollection(user.uid, boardId, layerId)
+      .get()
+      .then(querySnapshot => {
+        querySnapshot.docs.forEach(point => {
+          const newX = -point.data().y < 0 ? -point.data().y + 100 : -point.data().y;
+          const newY = point.data().x < 0 ? point.data().x + 100 : point.data().x;
+          point.ref.update({
+            x: newX,
+            y: newY,
+          });
+        });
+      });
+  };
+
   if (!layer.loaded || !image.loaded || !points.loaded) {
     return (
       <div className="main">
@@ -116,7 +168,14 @@ const Layer = () => {
   if (!image.exists) {
     return <UploadFile />;
   }
-  return <Map defaultLayer={layer.data} image={image.data} />;
+  return (
+    <>
+      <Button onClick={handleChange} tooltip="Повернуть по часовой стрелке">
+        <MdRotateRight size="25px" />
+      </Button>
+      <Map defaultLayer={layer.data} image={image.data} />
+    </>
+  );
 };
 
 export default Layer;
