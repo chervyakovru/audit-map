aba242a44563f9ec3c933f5d683919d148e3c63e
Daniil Chervyakov <daniil@chervyakov.ru>
2020-07-22 03:40:46 +0500

    pretttify context menu

--- src/CustomContextMenu/index.js
+++ src/CustomContextMenu/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { useOutsideClick } from '../utils';
 import CustomContextMenuContext from '../CustomContextMenuContext';
 
+import styles from './CustomContextMenu.module.css';
+
 const CustomContextMenu = ({ x, y, menuItems }) => {
   const customContextMenuRef = React.useRef(null);
   const { customContextMenuProps, setCustomContextMenuProps } = React.useContext(CustomContextMenuContext);
@@ -19,13 +21,13 @@ const CustomContextMenu = ({ x, y, menuItems }) => {
     <div
       ref={customContextMenuRef}
       style={{ left: x, top: y }}
-      className="uk-card uk-card-default uk-card-body uk-position-fixed uk-position-z-index"
+      className="uk-card uk-card-small uk-card-default uk-card-body uk-padding-remove-horizontal uk-position-fixed uk-position-z-index"
     >
-      <ul className="uk-list">
+      <ul className="uk-nav uk-dropdown-nav">
         {menuItems.map((item, index) => (
           // eslint-disable-next-line react/no-array-index-key
           <li key={index}>
-            <button type="button" onClick={getHandleMenuItemClick(item.onClick)}>
+            <button onClick={getHandleMenuItemClick(item.onClick)} type="button" className={styles.listElement}>
               {item.label}
             </button>
           </li>
